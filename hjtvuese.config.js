96c25585b2dea7e12b6b166d508c4281120d0b8f
qiuquan <18306485667@163.com>
2022-10-01 14:04:58 +0800

    🐞 fix(修改组件文档生成位置):

--- hjtvuese.config.js
+++ hjtvuese.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'left-capsule组件文档', // 生成组件文档index的title
+  title: '三翼鸟view组件文档', // 生成组件文档index的title
   include: ['src/components/**/*.vue'], // 包含的文件
   exclude: [], // 排除的文件，node_modules已被默认排除
-  outDir: './docs', // 文档的输出目录
+  outDir: './packages/docs', // 文档的输出目录
 }
