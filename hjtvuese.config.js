737585326ca0a22918e8bd0bf506860656fecbcf
qiuquan <18306485667@163.com>
2022-10-01 14:18:03 +0800

    fix:修改路径

--- hjtvuese.config.js
+++ hjtvuese.config.js
@@ -2,5 +2,5 @@ module.exports = {
   title: '三翼鸟view组件文档', // 生成组件文档index的title
   include: ['src/components/**/*.vue'], // 包含的文件
   exclude: [], // 排除的文件，node_modules已被默认排除
-  outDir: './packages/docs', // 文档的输出目录
+  outDir: './docs', // 文档的输出目录
 }
